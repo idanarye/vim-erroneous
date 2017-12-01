7b1a922f8f90e25da78abbdedfeaae0fb1f13f4a
IdanArye <idanarye@gmail.com>
2017-12-01 02:55:18 +0200

    Change Omnipytent interface to use shell executor parameters

--- omnipytent/erroneous.py
+++ omnipytent/erroneous.py
@@ -1,12 +1,22 @@
-from omnipytent import CMD
+from omnipytent import FN
 from omnipytent.execution import ShellCommandExecuter
 
 
-@ShellCommandExecuter
-def ERUN(command):
-    CMD.Erun.bang(command)
+class TargetList:
+    QUICKFIX = 1
+    LOCATION = 2
 
 
-@ShellCommandExecuter
-def ELRUN(command):
-    CMD.Elrun.bang(command)
+class ERUN(ShellCommandExecuter):
+    @property
+    def loc(self):
+        return self(target_list=TargetList.LOCATION)
+
+    @property
+    def bang(self):
+        return self(jump=False)
+
+
+@ERUN
+def ERUN(command, target_list=TargetList.QUICKFIX, jump=True):
+    return FN['erroneous#run'](command, True, target_list, jump)
